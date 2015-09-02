961f7ee1e6937e8eedee065e04e05ec9fa87934d
Stefan Gehrig <gehrig@teqneers.de>
2015-09-02 10:14:07 +0200

    fixed configuration test

--- tests/DependencyInjection/ConfigurationTest.php
+++ tests/DependencyInjection/ConfigurationTest.php
@@ -32,10 +32,15 @@ class ConfigurationTest extends \PHPUnit_Framework_TestCase
         $processor     = new Processor();
         $config        = $processor->processConfiguration($configuration, array(
             array(
-                'builds' => array(
+                'app_path' => '%kernel.root_dir%/../ExampleApp',
+                'builds'   => array(
                     'desktop' => array(
-                        'development_base' => 'my-app',
-                        'production_base'  => 'app'
+                        'development' => array(
+                            'build_path' => 'build/development/ExampleApp'
+                        ),
+                        'production'  => array(
+                            'build_path' => 'build/production/ExampleApp'
+                        )
                     )
                 )
             )
@@ -43,20 +48,21 @@ class ConfigurationTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals(
             array(
-                'workspace_path'          => '%kernel.root_dir%/../workspace',
-                'relative_wWorkspace_url' => '../workspace',
-                'web_path'                => '%kernel.root_dir%/../web',
-                'relative_web_url'        => '/',
-                'builds'                  => array(
+                'app_path' => '%kernel.root_dir%/../ExampleApp',
+                'builds'   => array(
                     'desktop' => array(
-                        'development_base'        => 'my-app',
-                        'production_base'         => 'app',
-                        'development_manifest'    => 'manifest.json',
-                        'development_microloader' => 'bootstrap.js',
-                        'development_appcache'    => null,
-                        'production_manifest'     => 'bootstrap.json',
-                        'production_microloader'  => 'bootstrap.js',
-                        'production_appcache'     => 'cache.appcache',
+                        'development' => array(
+                            'build_path'  => 'build/development/ExampleApp',
+                            'microloader' => '/bootstrap.js',
+                            'manifest'    => '/bootstrap.json',
+                            'app_cache'   => null
+                        ),
+                        'production'  => array(
+                            'build_path'  => 'build/production/ExampleApp',
+                            'microloader' => 'microloader.js',
+                            'manifest'    => 'app.json',
+                            'app_cache'   => 'cache.appcache'
+                        )
                     )
                 )
             ),
@@ -64,7 +70,20 @@ class ConfigurationTest extends \PHPUnit_Framework_TestCase
         );
     }
 
-    public function testConfigurationInvalidIfAppNameIsMissing()
+    public function testConfigurationInvalidIfAppPathIsMissing()
+    {
+        $configuration = $this->getConfiguration();
+        $processor     = new Processor();
+
+        $this->setExpectedException(
+            'Symfony\Component\Config\Definition\Exception\InvalidConfigurationException',
+            'The child node "app_path" at path "tq_ext_js_application" must be configured.'
+        );
+
+        $processor->processConfiguration($configuration, array(array()));
+    }
+
+    public function testConfigurationInvalidIfBuildIsMissing()
     {
         $configuration = $this->getConfiguration();
         $processor     = new Processor();
@@ -74,6 +93,10 @@ class ConfigurationTest extends \PHPUnit_Framework_TestCase
             'The child node "builds" at path "tq_ext_js_application" must be configured.'
         );
 
-        $processor->processConfiguration($configuration, array(array()));
+        $processor->processConfiguration($configuration, array(
+            array(
+                'app_path' => '%kernel.root_dir%/../ExampleApp'
+            )
+        ));
     }
 }
