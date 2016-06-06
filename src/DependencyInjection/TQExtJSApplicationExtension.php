80720ef3f89f5cbd3875feebd91adb9a2d5ad61e
Stefan Gehrig <gehrig@teqneers.de>
2016-06-06 10:58:41 +0200

    added parameter to application path

--- src/DependencyInjection/TQExtJSApplicationExtension.php
+++ src/DependencyInjection/TQExtJSApplicationExtension.php
@@ -32,15 +32,19 @@ class TQExtJSApplicationExtension extends Extension
         $configuration = $this->getConfiguration($config, $container);
         $config        = $this->processConfiguration($configuration, $config);
 
+        $container->setParameter('tq_extjs.app_path', $config['app_path']);
+
         $applicationDefinition = $container->getDefinition('tq_extjs.application_configuration');
-        $applicationDefinition->replaceArgument(0, $config['app_path']);
 
         foreach ($config['builds'] as $name => $build) {
-            $applicationDefinition->addMethodCall('addBuild', [
-                $name,
-                $build['development'],
-                $build['production']
-            ]);
+            $applicationDefinition->addMethodCall(
+                'addBuild',
+                [
+                    $name,
+                    $build['development'],
+                    $build['production'],
+                ]
+            );
         }
     }
 }
